5823f28ff17e4f97f8a0079f4724ec3b678a680a
Veselin Petev Vasilev <veselin95@hotmail.es>
2016-04-25 12:33:52 +0200

    Author y footer

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author. */
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: 'author' });
+});
+
 module.exports = router;
