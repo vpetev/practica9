8f5acd5fe6fa217030eb2ae9a89e0eebbe1d5fad
Veselin Petev Vasilev <veselin95@hotmail.es>
2016-05-01 19:08:37 +0200

    Tema 3: Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/question', quizController.question);
+router.get('/check', quizController.check);
 
 /* GET author. */
 router.get('/author', function(req, res, next) {
