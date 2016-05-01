9d1cbc02871ec1823869f96bd6938a2099632bef
Veselin Petev Vasilev <veselin95@hotmail.es>
2016-05-01 19:19:30 +0200

    pregunta mejorada

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,13 @@
+//GET /question
+exports.question = function(req, res, next){
+	var answer = req.query.answer || '';
+	res.render('quizzes/question', {question: 'Capital de Italia', answer: answer});
+};
+
+//GET /check
+exports.check = function(req, res, next){
+
+	var answer = req.query.answer || "";
+	var result = ((answer === 'Roma') ? 'Correcta' : 'Incorrecta');
+	res.render('quizzes/result', {result: result, answer:answer});
+};
